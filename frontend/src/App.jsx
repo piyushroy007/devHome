ff3b390c51947cf451060bd756c181d79dfd5ecc
piyushroy007 <piyush9051roy@gmail.com>
2026-04-01 23:47:02 +0530

    navBar setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 
 // Placeholder Components
 const Home = () => (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-        <div className="navbar bg-gray-300 px-5">
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="flex-none gap-2">
-                <div className="dropdown dropdown-end">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
-                        </div>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a>Settings</a>
-                        </li>
-                        <li>
-                            <a>Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+    <>
+        <NavBar />
+        <div>
+            <h1 className="text-3xl font-bold text-gray-800 border-b pb-2 mb-6">
+                Welcome to devHome
+            </h1>
+            <p className="text-lg text-gray-700">Find your coding match!</p>
         </div>
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">
-            Welcome to devHome
-        </h1>
-        <p className="text-lg text-gray-700">Find your coding match!</p>
-        <div className="mt-6 space-x-4">
-            <button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-                Login
-            </button>
-            <button className="px-6 py-2 border border-blue-500 text-blue-500 rounded-lg hover:bg-blue-50 transition">
-                Signup
-            </button>
-        </div>
-    </div>
+    </>
 );
 
 const Dashboard = () => (
